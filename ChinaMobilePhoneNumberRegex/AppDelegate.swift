e7633252bf4a34def00a5997c4fc20e6ef290480
VincentXue <vincentxueios@gmail.com>
2014-12-19 00:51:30 +0800

    Initial Commit

---
+++ ChinaMobilePhoneNumberRegex/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ChinaMobilePhoneNumberRegex
+//
+//  Created by Vincent Sit on 14/12/19.
+//  Copyright (c) 2014年 Vincent Sit. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ ChinaMobilePhoneNumberRegexTests/ChinaMobilePhoneNumberRegexTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChinaMobilePhoneNumberRegexTests.swift
+//  ChinaMobilePhoneNumberRegexTests
+//
+//  Created by Vincent Sit on 14/12/19.
+//  Copyright (c) 2014年 Vincent Sit. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class ChinaMobilePhoneNumberRegexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
